16955e2d644d3d7055b12682609b0e67489f5f34
animettion <leonardo.kenny@gmail.com>
2019-04-10 00:17:04 -0300

    form

--- proagil-app/src/app/app.module.ts
+++ proagil-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +26,8 @@ import { BsDropdownModule, ModalModule, TooltipModule } from 'ngx-bootstrap';
       FormsModule,
       BsDropdownModule.forRoot(),
       TooltipModule.forRoot(),
-      ModalModule.forRoot()
+      ModalModule.forRoot(),
+      ReactiveFormsModule
    ],
    providers: [
       EventoService
--- proagil-app/src/app/eventos/eventos.component.ts
+++ proagil-app/src/app/eventos/eventos.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { EventoService } from '../_services/evento.service';
-import { Evento } from '../_models/Evento';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { Component, OnInit, TemplateRef } from "@angular/core";
+import { EventoService } from "../_services/evento.service";
+import { Evento } from "../_models/Evento";
+import { BsModalRef, BsModalService } from "ngx-bootstrap";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-eventos',
-  templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.css']
+  selector: "app-eventos",
+  templateUrl: "./eventos.component.html",
+  styleUrls: ["./eventos.component.css"]
 })
 export class EventosComponent implements OnInit {
   // tslint:disable-next-line:variable-name
-  _filtroLista = '';
+  _filtroLista = "";
   eventosFiltrados: any = [];
   eventos: Evento[];
   imagemLargura = 50;
   imagemMargem = 50;
   mostrarImagem = false;
   modalRef: BsModalRef;
+  registerForm: FormGroup;
 
   get filtroLista(): string {
     return this._filtroLista;
@@ -30,12 +32,12 @@ export class EventosComponent implements OnInit {
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
-
   }
 
-
-  constructor(private eventoService: EventoService
-    ,         private modalService: BsModalService) {}
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService
+  ) {}
 
   ngOnInit() {
     this.getEventos();
@@ -54,10 +56,23 @@ export class EventosComponent implements OnInit {
     this.mostrarImagem = !this.mostrarImagem;
   }
 
+  validation() {
+    this.registerForm = new FormGroup({
+      Tma: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
+      Local: new FormControl('', Validators.required),
+      ImagemURL: new FormControl('', Validators.required),
+      DataEvento: new FormControl('', Validators.required),
+      QtdPessoas: new FormControl('', [Validators.required, Validators.max(12000)]),
+      Telefone: new FormControl('', Validators.required),
+      Email: new FormControl('', [Validators.required, Validators.email])
+    });
+  }
+
+  salvarAlteracao() {}
   getEventos() {
     this.eventoService.getAllEvento().subscribe(
-// tslint:disable-next-line: variable-name
-  (_eventos: Evento[]) => {
+      // tslint:disable-next-line: variable-name
+      (_eventos: Evento[]) => {
         this.eventos = _eventos;
         this.eventosFiltrados = this.eventos;
         console.log(_eventos);
